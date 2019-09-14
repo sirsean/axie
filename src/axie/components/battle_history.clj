df33f849234931ae57c9abf2430efa33cd9851af
Sean Schulte <sirsean@gmail.com>
2019-09-14 17:00:46 -0500

    indentation

--- src/axie/components/battle_history.clj
+++ src/axie/components/battle_history.clj
@@ -41,16 +41,16 @@
           (loop [battle-id (inc max-battle-id)]
             (if (running?)
               (let [battle @(api/fetch-battle battle-id)]
-              (if (and (some? battle) (not= {:script nil} battle))
-                (let [{:keys [winner loser]} (bh/battle->teams battle)]
-                  (log/infof "battle %s, winner: %s, loser: %s" battle-id winner loser)
-                  (when (some? winner)
-                    (increment-wins winner))
-                  (when (some? loser)
-                    (increment-losses loser))
-                  (bh/set-max-battle-id battle-id)
-                  (recur (inc battle-id)))
-                (dec battle-id)))
+                (if (and (some? battle) (not= {:script nil} battle))
+                  (let [{:keys [winner loser]} (bh/battle->teams battle)]
+                    (log/infof "battle %s, winner: %s, loser: %s" battle-id winner loser)
+                    (when (some? winner)
+                      (increment-wins winner))
+                    (when (some? loser)
+                      (increment-losses loser))
+                    (bh/set-max-battle-id battle-id)
+                    (recur (inc battle-id)))
+                  (dec battle-id)))
               (do
                 (log/infof "stopping early!")
                 (dec battle-id))))]
